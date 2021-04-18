ff7970f15bd6cb574e6e6b1fad6e54b96e7d7c40
elleven11 <elleven11@github.com>
2021-04-18 16:46:36 -0400

    Changed user model

--- api/models/User.go
+++ api/models/User.go
@@ -12,6 +12,7 @@ import (
 )
 
 type User struct {
+	gorm.Model
 	ID       uint32 `gorm:"primary_key;auto_increment" json:"id"`
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Email    string `gorm:"size:100;not null;unique" json:"email"`
