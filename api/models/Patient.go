638937cf9668f767b449a4107090f9b09398a5d1
elleven11 <elleven11@github.com>
2021-04-18 14:34:23 -0400

    patient model

--- api/models/Patient.go
+++ api/models/Patient.go
@@ -1,19 +1,21 @@
 package models
 
+import "github.com/lib/pq"
+
 type Patient struct {
-	ID        		uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	Pathogenic  	bool	  `gorm:"" json:"pathogenic"`
-	Gene			string	  `gorm:"" json:"gene"`
-	Ethnicity		string	  `gorm:"" json:"ethnicity"`
-	CancerDx		bool	  `gorm:"" json:"cancer_dx"`
-	CancerDxType	string	  `gorm:"" json:"cancer_dx_type"`
-	CancerDxAge		int		  `gorm:"" json:"cancer_dx_age"`
-	KnownCancer		string	  `gorm:"" json:"known_cancer"`
-	RelRelation		[]string  `gorm:"" json:"rel_relation"`
-	RelCancer		[]string  `gorm:"" json:"rel_cancer"`
-	RelAge			[]string  `gorm:"" json:"rel_age"`
-	IndexRel		float64   `gorm:"" json:"index_rel"`
-	Prediction		float64	  `gorm:"" json:"prediction"`
-	User      		User      `json:"user"`
-	UserID    		uint32    `gorm:"not null" json:"user_id"`
+	ID           uint64         `gorm:"primary_key;auto_increment" json:"id"`
+	Pathogenic   bool           `gorm:"default:false" json:"pathogenic"`
+	Gene         string         `gorm:"size:100" json:"gene"`
+	Ethnicity    string         `gorm:"size:255" json:"ethnicity"`
+	CancerDx     string         `gorm:"size:100" json:"cancer_dx"`
+	CancerDxType string         `gorm:"size:255" json:"cancer_dx_type"`
+	CancerDxAge  int64          `json:"cancer_dx_age"`
+	KnownBrca    string         `gorm:"size:255" json:"known_brca"`
+	KnownCancer  string         `gorm:"size:100" json:"known_cancer"`
+	RelRelation  pq.StringArray `gorm:"type:string[]" json:"rel_relation"`
+	RelCancer    pq.StringArray `gorm:"type:string[]" json:"rel_cancer"`
+	RelAge       pq.StringArray `gorm:"type:string[]" json:"rel_age"`
+	Prediction   float64        `gorm:"" json:"prediction"`
+	User         User           `json:"user"`
+	UserID       uint32         `gorm:"not null" json:"user_id"`
 }
