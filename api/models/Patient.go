9908eed968c7a5d19f0468d0ce7bf554310c6387
elleven11 <elleven11@github.com>
2021-04-18 16:45:40 -0400

    Patient model created

--- api/models/Patient.go
+++ api/models/Patient.go
@@ -1,21 +1,130 @@
 package models
 
-import "github.com/lib/pq"
+import (
+	"errors"
+	"html"
+
+	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
+)
 
 type Patient struct {
+	gorm.Model
 	ID           uint64         `gorm:"primary_key;auto_increment" json:"id"`
-	Pathogenic   bool           `gorm:"default:false" json:"pathogenic"`
-	Gene         string         `gorm:"size:100" json:"gene"`
 	Ethnicity    string         `gorm:"size:255" json:"ethnicity"`
 	CancerDx     string         `gorm:"size:100" json:"cancer_dx"`
 	CancerDxType string         `gorm:"size:255" json:"cancer_dx_type"`
 	CancerDxAge  int64          `json:"cancer_dx_age"`
-	KnownBrca    string         `gorm:"size:255" json:"known_brca"`
-	KnownCancer  string         `gorm:"size:100" json:"known_cancer"`
 	RelRelation  pq.StringArray `gorm:"type:string[]" json:"rel_relation"`
 	RelCancer    pq.StringArray `gorm:"type:string[]" json:"rel_cancer"`
 	RelAge       pq.StringArray `gorm:"type:string[]" json:"rel_age"`
-	Prediction   float64        `gorm:"" json:"prediction"`
+	Prediction   float64        `json:"prediction"`
 	User         User           `json:"user"`
 	UserID       uint32         `gorm:"not null" json:"user_id"`
+}
+
+func (p *Patient) Prepare() {
+	p.ID = 0
+	p.User = User{}
+
+	// escaping html to prevent SQLi
+	p.Ethnicity = html.EscapeString(p.Ethnicity)
+	p.CancerDx = html.EscapeString(p.CancerDx)
+	p.CancerDxType = html.EscapeString(p.CancerDxType)
+
+	// had to iterate 3 times because we are doing Prepare before Validate
+	for i := 0; i < len(p.RelRelation); i++ {
+		p.RelRelation[i] = html.EscapeString(p.RelRelation[i])
+	}
+	for i := 0; i < len(p.RelCancer); i++ {
+		p.RelCancer[i] = html.EscapeString(p.RelCancer[i])
+	}
+	for i := 0; i < len(p.RelAge); i++ {
+		p.RelAge[i] = html.EscapeString(p.RelAge[i])
+	}
+}
+
+func (p *Patient) Validate() error {
+
+	if p.CancerDx == "" {
+		return errors.New("CancerDx is required")
+	}
+	if p.UserID < 1 {
+		return errors.New("An user is required")
+	}
+
+	if len(p.RelRelation) != len(p.RelCancer) {
+		return errors.New("RelRelation length does not match RelCancer")
+	}
+	if len(p.RelRelation) != len(p.RelAge) {
+		return errors.New("RelRelation length does not match RelAge")
+	}
+
+	return nil
+}
+
+func (p *Patient) SavePatient(db *gorm.DB) (*Patient, error) {
+	var err error
+	err = db.Debug().Model(&Patient{}).Create(&p).Error
+	if err != nil {
+		return &Patient{}, err
+	}
+
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.UserID).Take(&p.User).Error
+		if err != nil {
+			return &Patient{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Patient) FindAllPatients(db *gorm.DB) (*[]Patient, error) {
+	var err error
+	patients := []Patient{}
+
+	err = db.Debug().Model(&Patient{}).Limit(100).Find(&patients).Error
+	if err != nil {
+		return &[]Patient{}, err
+	}
+
+	if len(patients) > 0 {
+		for i, _ := range patients {
+			err := db.Debug().Model(&User{}).Where("id = ?", patients[i].UserID).Take(&patients[i].User).Error
+			if err != nil {
+				return &[]Patient{}, err
+			}
+		}
+	}
+	return &patients, nil
+}
+
+func (p *Patient) FindPatientByID(db *gorm.DB, pat_id uint64) (*Patient, error) {
+	var err error
+
+	err = db.Debug().Model(&Patient{}).Where("id = ?", pat_id).Take(&p).Error
+	if err != nil {
+		return &Patient{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.UserID).Take(&p.User).Error
+		if err != nil {
+			return &Patient{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Patient) DeletePatient(db *gorm.DB, pat_id uint64, user_id uint32) (int64, error) {
+
+	db = db.Debug().Model(&Patient{}).Where("id = ? and user_id = ?", pat_id, user_id).Take(&Patient{}).Delete(&Patient{})
+
+	if db.Error != nil {
+		// if it does not find the Patient
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Patient was not found")
+		}
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
 }
