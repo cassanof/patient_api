f3005cd4d9e858d18aab18f621d98a1b71ec41b5
elleven11 <elleven11@github.com>
2021-04-19 12:38:04 -0400

    initial routes

---
+++ api/controllers/routes.go
@@ -0,0 +1,24 @@
+package controllers
+
+import "github.com/elleven11/patient_api/api/middlewares"
+
+func (srv *Server) initRoutes() {
+
+	srv.Router.HandleFunc("/", middlewares.SetJSON(srv.Home)).Methods("GET")
+	srv.Router.HandleFunc("/login", middlewares.SetJSON(srv.Login)).Methods("POST")
+
+	// Users
+	srv.Router.HandleFunc("/users", middlewares.SetJSON(srv.CreateUser)).Methods("POST")
+	srv.Router.HandleFunc("/users", middlewares.SetJSON(srv.GetUsers)).Methods("GET")
+	srv.Router.HandleFunc("/users/{id}", middlewares.SetJSON(srv.GetUser)).Methods("GET")
+	srv.Router.HandleFunc("/users/{id}", middlewares.SetJSON(middlewares.SetAuth(srv.UpdateUser))).Methods("PUT")
+	srv.Router.HandleFunc("/users/{id}", middlewares.SetAuth(srv.DeleteUser)).Methods("DELETE")
+
+	// Patients
+	srv.Router.HandleFunc("/patients", middlewares.SetJSON(srv.CreatePatient)).Methods("POST")
+	srv.Router.HandleFunc("/patients", middlewares.SetJSON(srv.GetPatients)).Methods("GET")
+	srv.Router.HandleFunc("/patients/{id}", middlewares.SetJSON(srv.GetPatient)).Methods("GET")
+	srv.Router.HandleFunc("/patients/{id}", middlewares.SetJSON(middlewares.SetAuth(srv.UpdatePatient))).Methods("PUT")
+	srv.Router.HandleFunc("/patients/{id}", middlewares.SetAuth(srv.DeleteUser)).Methods("DELETE")
+
+}
