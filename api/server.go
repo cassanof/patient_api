24b12ee53774ffe6516725d3042000f06e0de578
elleven11 <elleven11@github.com>
2021-04-19 22:42:07 -0400

    server.go

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/elleven11/patient_api/api/controllers"
+	"github.com/elleven11/patient_api/api/seed"
+	"github.com/joho/godotenv"
+)
+
+var srv = controllers.Server{}
+
+func Start() {
+	var err error
+	fmt.Println("Starting api...")
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Couldn't get env... Error: %v", err)
+	} else {
+		fmt.Println("Got env...")
+	}
+
+	srv.Init(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(srv.DB)
+
+	srv.Run(":8080")
+}
