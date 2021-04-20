e3bf1ad77582e499a90ca32df92782fe1e4b7b9a
elleven11 <elleven11@github.com>
2021-04-19 23:41:10 -0400

    Changed string[] to text[]

--- api/models/Patient.go
+++ api/models/Patient.go
@@ -9,15 +9,14 @@ import (
 )
 
 type Patient struct {
-	gorm.Model
 	ID           uint64         `gorm:"primary_key;auto_increment" json:"id"`
 	Ethnicity    string         `gorm:"size:255" json:"ethnicity"`
 	CancerDx     string         `gorm:"size:100" json:"cancer_dx"`
 	CancerDxType string         `gorm:"size:255" json:"cancer_dx_type"`
 	CancerDxAge  int64          `json:"cancer_dx_age"`
-	RelRelation  pq.StringArray `gorm:"type:string[]" json:"rel_relation"`
-	RelCancer    pq.StringArray `gorm:"type:string[]" json:"rel_cancer"`
-	RelAge       pq.StringArray `gorm:"type:string[]" json:"rel_age"`
+	RelRelation  pq.StringArray `gorm:"type:text[]" json:"rel_relation"`
+	RelCancer    pq.StringArray `gorm:"type:text[]" json:"rel_cancer"`
+	RelAge       pq.StringArray `gorm:"type:text[]" json:"rel_age"`
 	Prediction   float64        `json:"prediction"`
 	User         User           `json:"user"`
 	UserID       uint32         `gorm:"not null" json:"user_id"`
